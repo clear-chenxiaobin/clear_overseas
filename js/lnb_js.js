07bd65940b7c37b25b856bdc46fd4feab94460b4
ningbo16 <liunb2012@yeah.net>
2017-01-06 10:05:53 +0800

    caseList 移动端手势库

--- js/lnb_js.js
+++ js/lnb_js.js
@@ -1,55 +1,58 @@
-(function($,window) {
-		var myCarousel = $(".myCarousel");
-		var body = $(body);
-		console.log(window)
-		console.log($("window"))
-	    console.log($(window).height())
-	    var window =$(window)
-		console.log(body.innerWidth(),body.width(),body.outerWidth(),window.height())
-	    var preIndex = 0;
-	    var indicators= $('.carousel-indicators li')
-	    var items= $('.carousel-inner .item')
-	    // 点击弹出
-	    $('.percase').click(function(event) {
-	         // Act on the event 
-	        var nowIndex = $(this).index();
-            var windowH = $(window).height();
-            var windowW = $(window).width();
+(function($, window) {
+        // console.log(screen.width)
+        // 768以下，判断为移动端 
+        if (screen.width < 768) {
 
-	        // 所有的class清空
-	        indicators.each(function(index, el) {
-	        	el.className = '';
-	        });
-	        items.each(function(index, el) {
-	        	el.className = 'item';
-	        });
-	        
+            $('.ca-modal-close').css('display', 'none');
+            var divWidth = screen.width;
+            $('.ca-modal-dialog').css('width', divWidth + 'px');
+            $('#myCarousel .carousel-inner .col-md-7 ').each(function(index, el) {
+                $(el).css({
+                    'width': divWidth + 'px',
+                    'height': divWidth * 0.77 + 'px'
+                });
+            });
+        }
 
-	        indicators[nowIndex].className ='active';
-	        items[nowIndex].className +=' active';
-	        document.getElementById('modelflag').click();
-            // $('.modelFlag').click();
-	        console.log(windowH,windowW)
-	        // myCarousel.css('position', 'fixed');
-	        // myCarousel.animate({
-	        // 	'width': (windowW - 200),
-	        // 	'height': (windowH - 200),
-	        // 	'duration':100,
-	        // 	"top": 100,
-	        // 	"left":100,
-	        // 	/* stuff to do after animation is complete */
-	        // });
-	    });
-	    // // 关闭按钮
-	    // $('.closeCarousel').click(function(event) {
-	    // 	/* Act on the event */
-	    // 	myCarousel.animate({
-	    // 		'width': '0px',
-	    // 		'height': '0px',
-	    // 		'duration':'500',
-	    // 		"top": '50%',
-	    // 		"left":'50%'
-	    // 		/* stuff to do after animation is complete */
-	    // 	});
-	    // });
-})($,window)
+        // 清除弹窗的右边距
+        document.getElementById("myModal").style.paddingRight = "0px";
+
+        var myCarousel = $(".myCarousel");
+        var body = $(body);
+        var preIndex = 0;
+        var indicators = $('.carousel-indicators li')
+        var items = $('.carousel-inner .item')
+            // 点击弹出
+        $('.percase').click(function(event) {
+            // Act on the event 
+            var nowIndex = $(this).index();
+            // var windowH = $(window).height();
+            // var windowW = $(window).width();
+
+            // 所有的class清空
+            indicators.each(function(index, el) {
+                el.className = '';
+            });
+            items.each(function(index, el) {
+                el.className = 'item';
+            });
+            indicators[nowIndex].className = 'active';
+            items[nowIndex].className += ' active';
+            // 弹出框
+            document.getElementById('modelflag').click();
+        });
+
+        // 触发 点击 关闭弹窗 事件
+        $('.ca-modal-close, .ca-phone-arrow').click(function(event) {
+            $('#myModal').trigger('click')
+        });
+
+        // 触发 移动端 左右滑动
+        $('#myModal .carousel-inner .item').on("swipeleft", function() {
+                $('.left.carousel-control.ca-arrow').trigger('click')
+        });
+        $('#myModal .carousel-inner .item').on("swiperight", function() {
+                $('.right.carousel-control.ca-arrow').trigger('click')
+        });
+
+})($, window)
